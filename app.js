13da35a1771b9561163905cc5847d4d4ade29db3
chokobae <104503804+chokobae@users.noreply.github.com>
2022-05-06 12:01:11 +0900

    웹사이트 완성!!!!

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form ");
+
+
+function onLoginSub() {
+    tomato.preventDefault();
+    console.log(tomato);
+}
+
+loginForm.addEventListener("submit", onLoginSub);
